7ffd63e8f631bc82f610191e5214f1bc1b6cd333
thedrpepper7 <leon.grant1410@gmail.com>
2025-05-21 12:28:13 +0100

    close to finito
    just update the css js and html buttons on the object

--- js/snippets.js
+++ js/snippets.js
@@ -1,66 +1,90 @@
-const animatedtext = {
-    url: `img/animatedtext.png`,
-    description: `How I animated the header`,
-}
-
-const mobilemenu = {
-    url: `img/mobilemenu.png`,
-    description: `The burger menu that appears on mobile`,
-}
-
-const contactmenu = {
-    url: `img/contactmenu.png`,
-    description: `my contact menu`,
-}
-
-const codesnippets = {
-    url: `img/codesnippets.png`,
-    description: `This whole page`,
-}
-
-const colorchangesnippet = {
-    url: `img/colorchange.png`,
-    description: `How the colors change`,
-}
-
-const objectupdate = {
-    url: `img/objectupdate.png`,
-    description: `Object updating`,
-}
-
 //where the image will appear
 const codeimg = document.querySelector('#codeimage');
 
 //description under 
 const description = document.querySelector('#codedescription');
 
+function addImg(which, btn, lang) {
+    btn.addEventListener('click', ()=> {
+        if (lang === `html`) {
+            codeimg.style.backgroundImage = `url(${which.html[0]})`;
+            description.innerHTML = which.html[1];
+        } else if (lang === `css`) {
+            codeimg.style.backgroundImage = `url(${which.css[0]})`;
+            description.innerHTML = which.css[1];
+        } else if (lang === `js`) {
+            codeimg.style.backgroundImage = `url(${which.javascript[0]})`;
+            description.innerHTML = which.javascript[1];
+        }
+      
+    })
+}
+
 function find(id){
-    found = document.querySelector(`#${id}`);
+    let found = document.querySelector(`#${id}`);
     return found;
 } 
 
 
-function addImg(which, btn) {
-    btn.addEventListener('click', ()=> {
-        codeimg.style.backgroundImage = `url(${which.url})`;
-        description.innerHTML = which.description;
-    })
+const animatedtext = {
+    html:       [`img/animatedtexthtml.png`, `how i made the html`,],
+    javascript: [`img/animatedtextjs.png`,`How i made the js`,],
 }
 
-const ab = find(`animatedtextsnippet`);
-addImg(animatedtext, ab);
 
-const mm = find(`mobilemenusnippet`);
-addImg(mobilemenu, mm);
+const mobilemenu = {
+    css:        [`img/mobilemenucss.png`, `how i made the css`,],
+    javascript: [`img/mobilemenujs.png`, `How i made the js`,],
+}
 
-const cm = find(`contactmenusnippet`);
-addImg(contactmenu, cm);
+const contactmenu = {
+    html:       [`img/contactmenuhtml.png`, `how i made the html`,],
+    css:        [`img/contactmenucss.png`, `how i made the css`,],
+    javascript: [`img/contactmenujs.png`, `How i made the js`,],
+}
 
-const cs = find(`codingexamplessnippet`);
-addImg(codesnippets, cs);
+const codesnippets = {
+    html:       [`img/animatedtexthtml.png`, `how i made the html`,],
+    css:        [`img/contactme.jpg`, `how i made the css`,],
+    javascript: [`img/codesnippets.png`,`How i made the js`,],
+}
 
-const ccs = find(`colorchangesnippet`);
-addImg(colorchangesnippet, ccs);
+const colorchange = {
+    javascript: [`img/colorchange.png`, `How i made the js`,]
+}
 
-const ou = find(`objectupdatesnippet`);
-addImg(objectupdate, ou);
+const objectupdate = {
+    javascript: [`img/objectupdate.png`, `How i made the js`,]
+}
+
+
+const athtml = find('animatedtexthtml');
+const atjs = find(`animatedtextjs`);
+addImg(animatedtext, athtml, `html`);
+addImg(animatedtext, atjs, `js`);
+
+const mmcss = find('mobilemenucss');
+const mmjs = find(`mobilemenujs`);
+addImg(mobilemenu, mmcss, `css`);
+addImg(mobilemenu, mmjs, `js`);
+
+const cmhtml = find(`contactmenuhtml`)
+const cmcss = find(`contactmenucss`);
+const cmjs = find(`contactmenujs`);
+addImg(contactmenu, cmhtml, `html`);
+addImg(contactmenu, cmcss, `css`);
+addImg(contactmenu, cmjs, `js`);
+
+const cehtml = find(`codingexampleshtml`)
+const cecss = find(`codingexamplescss`);
+const cejs = find(`codingexamplesjs`);
+addImg(codesnippets, cehtml, `html`);
+addImg(codesnippets, cecss, `css`);
+addImg(codesnippets, cejs, `js`);
+
+const ccjs = find('colorchangejs');
+addImg(colorchange, ccjs, `js`);
+
+
+const oujs = find(`objectupdate`);
+addImg(objectupdate, oujs, `js`);
